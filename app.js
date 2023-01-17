ab50eb07c7843a1b7d37685279cf390815cd5118
Timur <elecktrohachyck@gmail.com>
2023-01-17 21:17:56 +0400

    numbers

--- app.js
+++ app.js
@@ -1,182 +1,72 @@
-// 1 Variables
+//1 Numbers
+//const num = 42 //integer
+//const float = 42.42 //float
+//const pow = 10e3 // pow
 
-//const firstName = 'Timur'
+//console.log('MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)
+//console.log('MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)
+//console.log('Math.pow', Math.pow(2, 53) - 1)
+//console.log('MAX_VALUE', Number.MAX_VALUE)
+//console.log('MIN_VALUE', Number.MIN_VALUE)
+//console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
+//console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
+//console.log(Number.NaN) //Not a Number, we can recieve it by doing wrong operations with numbers like in the next strings of code
+//const weird = 2 / undefined
+//console.log(Number.isNaN(weird))
+//console.log(Number.isNaN (42))
+//console.log(Number.isFinite(Infinity))
+//console.log(Number.isFinite(42))
 
-//const age = 26 //number
-//const isProgrammer = true //boolean
+//const stringInt = '40'
+//const stringFloat = '40.42'
 
-//const _ = 'some'
-//const $ = 'thing'
-//const withNum5 = '5'
+//console.log(Number.parseInt(stringInt) + 2) //parseInt or Float lets you turn a string into a number
+//console.log(parseInt(stringInt) + 2) //a standart of typing this one
+//console.log(Number(stringInt) + 2) //in next two examples you can see how to do it easier
+//console.log(+stringInt + 2)
 
-//const if = 'mkef' //error because key words aren't allowed to be used as variables
-//const 5withNum = '5' //error beacuse it's prohibited to create variables starting with number
+//console.log(parseInt(stringFloat) + 2) //parseInt works with only integers, so in this case it just ignores numbers after dot.
+//console.log(parseFloat(stringFloat) + 2) 
+//console.log(+stringFloat + 2) 
 
-
-// 2 Мутирование 
-//console.log('Human name is: ' + firstName + ', and his age is ' + age)
-//alert('Human name is: ' + firstName + ', and his age is ' + age)
-
-//const lastName = prompt('Введите фамилию')
-//alert(firstName + ' ' + lastName)
-
-//3 Операторы
-//let currentYear = 2023
-//const birhtYear = 1996
-
-//const a = 10
-//const b = 5
-
-//let c = 32
-//c = c + a
-//c = c -a 
-//c = c * a
-//c = c / a
-//c += a
-//c -= a
-//c *= a
-//c /= a
-
-//console.log(a + b)
-//console.log(a - b)
-//console.log(a * b)
-//console.log(a / b)
-//console.log(++currentYear) //оператор ++ инкрементирует значение переменной на 1 единицу (от англ. increment «увеличение») 
-//console.log(--currentYear) //оператор -- декрементирует значение переменной на 1 единицу (от англ. decrement «уменьшение»)
-//console.log(c)
-
-//Типы данных (5 - примитивные типы данных)
-//const isProgrammer = true
-//const name = 'Timur'
-//const age = 26
-//let x
-//null
-
-//console.log(typeof isProgrammer)
-//console.log(typeof name)
-//console.log(typeof age)
-//console.log(typeof x)
-//console.log(null)
+//console.log(0.4 + 0.2) //we anticipate to see 0.6, but instead of this we'll see this one: 0.6000000000000001. I'll read about it later
+//console.log((0.4 + 0.2).toFixed(1)) //toFixed lets us to eliminate numbers after dot. You just need to choose how many of them will be shown in a console. In this case I've chosen to show one number after dot. But it's not enough as we receive a string instead of a number.
+//console.log(parseFloat((0.4 + 0.2).toFixed(1))) //in order to turn it into a number we add 'parseFloat'
 
 
-// 5 Приоритет операторов (Operator precedence)
-//const fullAge = 26
-//const birthYear = 1996
-//const currentYear = 2023
+// 2 BigInt - a new type of data which is made to operate with big numbers (which are more than max-safe-integer)
+//console.log(90071992547409919999999n) //in order to create a bigint add 'n' at the end of a number
+//console.log(90071992547409919999999n - 9007199254740991999999n) //we can work with bigints
+//console.log(-90071992547409919999999.22222n) //bigint according to its name don't work with floats
 
-//const isFullAge = currentYear - birthYear >= fullAge 
-//console.log(isFullAge)
+//console.log(10n - 4) //we can't mix bigint with other types of data
+//console.log(parseInt(10n) - 4) //we can turn bigint into a number
+//console.log(10n - BigInt(4)) //or on the contrary we can turn a number into bigint
+//console.log(5n / 2n) //we'll see '2' because as we knew earlier bigint works with only integers
 
 
-// 6 If/else operators
-//const courseStatus = 'fail' // ready, fail, pending
+// 3 Math
+//console.log(Math.E)
+//console.log(Math.PI)
 
-//if (courseStatus === 'ready') {
-//  console.log('Course is ready now and you can attend it')
-//} else if (courseStatus === 'pending') {
-//  console.log('Course is being developed so far')
-//} else {
-//  console.log('Course has been messed up')
+//console.log(Math.sqrt(49)) //'sqrt' is a method which find a square root of any number 
+//console.log(Math.pow(5, 3)) //'pow' is a method which turn any number into some degree
+//console.log(Math.abs(-42)) //'abs' turns a number into its module
+//console.log(Math.max(12, 42, 89, 37, 5))
+//console.log(Math.min(12, 42, 89, 37, 5))
+//console.log(Math.floor(4.9)) //округляет всегда в меньшую сторону
+//console.log(Math.ceil(4.2)) //округляет всегда в большую сторону
+//console.log(Math.round(4.7)) //округляет число к ближайшему целому числу
+//console.log(Math.trunc(4.7)) //вщзвращает целую часть числа путём удаления всех дробных 
+//console.log(Math.random()) //выдаёт рандомное число
+
+
+// 4 Exapmple  -  I gotta figure out how iy works later!
+//function getRandomBetween (min, max) {
+//  return Math.floor(Math.random() * (max - min + 1) + min)  
 //}
 
-//const isReady = true
-
-//if (isReady) {
-//  console.log('Everything is set up!')
-//} else {
-//  console.log('Please, wait for a result')
-//}
-
-//it's equal to next string of code
-
-//isReady ? console.log('Everything is set up!') : console.log('Please, wait for a result') //Это тернарное выражение (с франц. ternarie - тройной, т.е. оператор в данном случае принимает сразу три значения)
-
-// About double and triple equals
-//const num1 = 42 //number
-//const num2 = '42' //string
-
-//console.log(num1 === num2)
-
-
-// 7 Logic operators https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operato
-
-
-//8 Functions
-//function calculateAge(year) {
-//  return 2023 - year
-//}
-
-//console.log(calculateAge(1996))
-//console.log(calculateAge(2013))
-//console.log(calculateAge(2002))
-
-//function logInfoAbout (name, year) {
-//  const age = calculateAge(year)
-
-//  age > 0 ? console.log('Человеку по имени ' + name + ' сейчас ' + age + ' лет.') : console.log('Actually, it is already future')
-  
-//}
-
-//logInfoAbout('Тимур', 1996)
-//logInfoAbout('Тимур', 2025)
-
-
-//9 Arrays (массивы)
-//const cars = ['Mazda', 'Mersedes', 'Ford'] //it's the simple yet proper way to create arrays
-//const cars = new Array('Mazda', 'Mersedes', 'Ford') //here's an alternative one to create arrays (in this case you can see how JS works)
-//console.log(cars)
-//console.log(cars[0]) //we can select an item of array by calling its index
-//console.log(cars.length) //we can find out length of an array using this construction
-
-//cars[0] = 'Porsche' //we can also edit elements in an array
-//console.log(cars)
-//cars[3] = 'Mazda' //we can even add elements in our array
-//cars[cars.length] = 'Audi' //it's the way to add your element in the end of your array
-//console.log(cars)
-
-
-// 10 Loops and iterations (циклы)
-//const cars = ['Mazda', 'Mersedes', 'Ford', 'Porsche']
-
-//for (let i = 0; i < cars.length; i++) {  
-//  const car = cars[i]
-//  console.log(car)
-//}
-
-//for (let car of cars) {  // as you can see it's a more simple way 
-//  console.log(car)
-//}
-
-
-// 11 Objects - groups of different types of elements. An object includes keys and values 
-//const person = {
-//  firstName: 'Timur',
-//  lastName: 'Birgalin',
-//  year: 1996,
-//  languages: ['Ru', 'En '], //it's an example of an array in an object
-//  hasWife: false,
-//  greet: function() { //it's an example of method included in our object
-//    console.log('greet from person')
-//  }
-//}
-
-//console.log(person.firstName) //we can call specific info about our object
-//console.log(person['lastName'])
-//const key = 'year'
-//console.log(person[key])
-//person.hasWife = true //we can edit our keys
-//console.log(person.hasWife) 
-//person.isProgrammer = true //we can also add any keys in our object
-//console.log(person['isProgrammer'])
-//console.log(person)
-//person.greet()
-
-
-
-
-
-
-
+//console.log(getRandomBetween(10, 42))
 
 
 
